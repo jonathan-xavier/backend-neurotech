6841b6fd7613e62e2811a9e603af65a4612ffca9
Jonathan <jonathanxavier556@gmail.com>
2021-08-17 11:59:18 -0300

    primeiro commit

---
+++ src/main/java/com/novocontactura/novocontactura/model/Contactura.java
@@ -0,0 +1,30 @@
+package com.novocontactura.novocontactura.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Contactura {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String email;
+	private String name;
+	private String phone;
+	
+	
+	
+	
+}
